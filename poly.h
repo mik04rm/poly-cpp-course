1508eb0da8d54137729964eccd8ab9035943808a
Mikołaj Woliński <mw438820@students.mimuw.edu.pl>
2024-11-24 21:47:45 +0100

    Add clang-format file

--- poly.h
+++ poly.h
@@ -7,10 +7,10 @@ template <typename T, std::size_t N = 0> class poly {
   public:
     constexpr std::size_t size() const { return N; }
 
-    constexpr T &operator[](std::size_t i) const { return coefs[i]; }
+    constexpr T& operator[](std::size_t i) const { return coefs[i]; }
 
     template <typename U, std::size_t M>
-    constexpr poly &operator+=(const poly<U, M> &other) {
+    constexpr poly& operator+=(const poly<U, M>& other) {
         std::size_t min_size = std::min(N, M);
         for (std::size_t i = 0; i < min_size; i++) {
             coefs[i] += other.coefs[i];
