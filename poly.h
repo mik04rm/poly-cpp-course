ed0bace8e6feda5b3ffafca25617b1956ea327a2
Mikołaj Woliński <mw438820@students.mimuw.edu.pl>
2024-12-05 20:17:44 +0100

    xd

--- poly.h
+++ poly.h
@@ -8,6 +8,23 @@
 
 #define DEBUG 1
 
+#if DEBUG
+    #define DBG_PRINT(x)                                           \
+        if (!std::is_constant_evaluated()) {                       \
+            std::cout << x << std::endl;                          \
+        }
+#else
+    #define DBG_PRINT(x)
+#endif
+
+template <typename U, typename T, std::size_t M, std::size_t N>
+concept PolyConvertible = (M <= N) && std::is_convertible_v<U, T>;
+
+template <typename T, typename U>
+concept ConvertibleTo = requires { 
+    std::common_type<T, U>::type; 
+};
+
 template <typename T, std::size_t N = 0> 
 class poly  {
 
@@ -17,35 +34,31 @@ private:
 public:
     
     constexpr poly() {
-        if (DEBUG)
-            std::cout << "empty constructor called" << std::endl;
+        DBG_PRINT("empty constructor called");
     }
 
     template <typename U, std::size_t M>
     constexpr poly(const poly<U, M>& other) 
-    requires ((M <= N) && std::is_convertible_v<U, T>) {
-        if (DEBUG) 
-            std::cout << "copy constructor called" << std::endl;
-        for (std::size_t i = 0; i < M; ++i) {
+    requires PolyConvertible<U, T, M, N> {
+        DBG_PRINT("copy constructor called");
+        for (std::size_t i = 0; i < M; i++) {
             this->coefs[i] = static_cast<T>(other.coefs[i]);
         }
     }
 
     template <typename U, std::size_t M>
     constexpr poly(poly<U, M>&& other) 
-    requires ((M <= N) && std::is_convertible_v<U, T>) {
-        if (DEBUG) 
-            std::cout << "move constructor called" << std::endl;
-        for (std::size_t i = 0; i < M; ++i) {
+    requires PolyConvertible<U, T, M, N> {
+        DBG_PRINT("move constructor called");
+        for (std::size_t i = 0; i < M; i++) {
             this->coefs[i] = static_cast<T>(std::move(other.coefs[i]));
         }
     }
 
     template <typename U>
     constexpr poly(U&& arg) 
-    requires std::convertible_to<U,T> {
-        if (DEBUG) 
-            std::cout << "single constructor called" << std::endl;
+    requires std::convertible_to<U, T> {
+        DBG_PRINT("single constructor called");
         static_assert(N >= 1, "N cannot be smaller than 1");
         this->coefs[0] = static_cast<T>(std::move(arg));
     }
@@ -54,14 +67,12 @@ public:
     constexpr poly(Args&&... args)
     requires (sizeof...(Args) <= N && (std::convertible_to<Args, T> && ...))
     : coefs{std::forward<Args>(args)...} {
-        if (DEBUG) 
-            std::cout << "variadic constructor called" << std::endl;
+        DBG_PRINT("variadic constructor called");
     }
 
     template <typename U, std::size_t M>
     constexpr static auto const_poly(const poly<U,M>& p) {
-        if (DEBUG) 
-            std::cout << "const_poly constructor called" << std::endl;
+        DBG_PRINT("const_poly constructor called");
         return poly<poly<U,M>, 1>{p};
     }
     
@@ -76,4 +87,31 @@ public:
     constexpr const T& operator[](std::size_t i) const {
         return coefs[i];
     }
+
+    // Copy assignment operator
+    template <typename U, std::size_t M>
+    constexpr poly& operator=(const poly<U, M>& other)
+    requires PolyConvertible<U, T, M, N> {
+        for (std::size_t i = 0; i < M; i++) {
+            coefs[i] = static_cast<T>(other.coefs[i]);
+        }
+        return *this;
+    }
+
+    // Move assignment operator
+    template <typename U, std::size_t M>
+    constexpr poly& operator=(poly<U, M>&& other)
+    requires PolyConvertible<U, T, M, N> {
+        for (std::size_t i = 0; i < M; i++) {
+            coefs[i] = static_cast<T>(std::move(other.coefs[i]));
+        }
+        return *this;
+    }
+
+    // operator+= and operator+ in progress
 };
+
+// Deduction guide for deducing T and N
+// perfect forwarding of arguments
+template <typename... Args>
+poly(Args&&...) -> poly<std::common_type_t<Args...>, sizeof...(Args)>;
--- poly_example.cpp
+++ poly_example.cpp
@@ -1,21 +1,55 @@
 #include "poly.h"
+#include <assert.h>
 
 using namespace std;
 
-int main() {
-    poly<int, 2> p(2137, 6969);
-    cout << "p done" << endl;
-    poly<poly<int, 2>, 1> q(p);
-    cout << "q done" << endl;
-    for (size_t i = 0; i < q.size(); i++) {
-        cout << q[i][0] << " " << q[i][1] << endl;
-    }
+template <typename T, std::size_t N>
+  constexpr bool operator==(const poly<T, N>& a, const poly<T, N>& b)
+  {
+    for (std::size_t i = 0; i < N; ++i)
+      if (!(a[i] == b[i]))
+        return false;
+    return true;
+  }
 
-    for (size_t i = 0; i < p.size(); i++) {
-        cout << p[i] << endl;
-    }
+
+int main() {
+    // poly<int, 2> p(2137, 6969);
+    // cout << "p done" << endl;
+    // poly<poly<int, 2>, 1> q(p);
+    // cout << "q done" << endl;
+    // for (size_t i = 0; i < q.size(); i++) {
+    //     cout << q[i][0] << " " << q[i][1] << endl;
+    // }
+
+    // for (size_t i = 0; i < p.size(); i++) {
+    //     cout << p[i] << endl;
+    // }
 
     // poly<int, 2> q(3, 4);
     // poly<int, 2> r = p + q;
     // return 0;
+
+
+    constexpr auto p = poly(2, 1);
+    static_assert(p.size() == 2 && p[0] == 2 && p[1] == 1);
+    static_assert(std::is_same_v<decltype(p), const poly<int, 2>>);
+
+    constexpr auto q = poly(poly(1.0, 2.0), 3.0, 4.0);
+    static_assert(std::is_same_v<decltype(q), const poly<poly<double, 2>, 3>>);
+    static_assert(q[0][0] == 1.0);
+    static_assert(q[0][1] == 2.0);
+    static_assert(q[1][0] == 3.0);
+    static_assert(q[1][1] == 0.0);
+    static_assert(q[2][0] == 4.0);
+    static_assert(q[2][1] == 0.0);
+
+    constexpr auto pp(p);
+    static_assert(p == pp);
+    constexpr auto qq(q);
+    static_assert(q == qq);
+
+    
 }
+
+// clang++ -Wall -Wextra -std=c++20 -O2 poly_example.cpp
