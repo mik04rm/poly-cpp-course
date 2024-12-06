3576f68033951778ee1b305cc8d1bcf31083fc3a
Mikołaj Woliński <mw438820@students.mimuw.edu.pl>
2024-12-06 21:08:38 +0100

    Add operator+= and operator+

--- poly.h
+++ poly.h
@@ -3,43 +3,38 @@
 #endif
 
 #include <array>
-#include <utility>
 #include <iostream>
+#include <utility>
 
 #define DEBUG 1
 
 #if DEBUG
-    #define DBG_PRINT(x)                                           \
-        if (!std::is_constant_evaluated()) {                       \
-            std::cout << x << std::endl;                          \
-        }
+#define DBG_PRINT(x)                                                           \
+    if (!std::is_constant_evaluated()) {                                       \
+        std::cout << x << std::endl;                                           \
+    }
 #else
-    #define DBG_PRINT(x)
+#define DBG_PRINT(x)
 #endif
 
 template <typename U, typename T, std::size_t M, std::size_t N>
 concept PolyConvertible = (M <= N) && std::is_convertible_v<U, T>;
 
-template <typename T, typename U>
-concept ConvertibleTo = requires { 
-    std::common_type<T, U>::type; 
-};
+// template <typename T, typename U>
+// concept ConvertibleTo = requires { std::common_type<T, U>::type; };
 
-template <typename T, std::size_t N = 0> 
-class poly  {
+template <typename T, std::size_t N = 0> class poly {
 
-private:
+  private:
     std::array<T, N> coefs{};
 
-public:
-    
-    constexpr poly() {
-        DBG_PRINT("empty constructor called");
-    }
+  public:
+
+    constexpr poly() { DBG_PRINT("empty constructor called"); }
 
     template <typename U, std::size_t M>
-    constexpr poly(const poly<U, M>& other) 
-    requires PolyConvertible<U, T, M, N> {
+        requires PolyConvertible<U, T, M, N>
+    constexpr poly(const poly<U, M>& other) {
         DBG_PRINT("copy constructor called");
         for (std::size_t i = 0; i < M; i++) {
             this->coefs[i] = static_cast<T>(other.coefs[i]);
@@ -47,8 +42,8 @@ public:
     }
 
     template <typename U, std::size_t M>
-    constexpr poly(poly<U, M>&& other) 
-    requires PolyConvertible<U, T, M, N> {
+        requires PolyConvertible<U, T, M, N>
+    constexpr poly(poly<U, M>&& other) {
         DBG_PRINT("move constructor called");
         for (std::size_t i = 0; i < M; i++) {
             this->coefs[i] = static_cast<T>(std::move(other.coefs[i]));
@@ -56,59 +51,80 @@ public:
     }
 
     template <typename U>
-    constexpr poly(U&& arg) 
-    requires std::convertible_to<U, T> {
+        requires std::convertible_to<U, T>
+    constexpr poly(U&& arg) {
         DBG_PRINT("single constructor called");
         static_assert(N >= 1, "N cannot be smaller than 1");
         this->coefs[0] = static_cast<T>(std::move(arg));
     }
 
     template <typename... Args>
-    constexpr poly(Args&&... args)
-    requires (sizeof...(Args) <= N && (std::convertible_to<Args, T> && ...))
-    : coefs{std::forward<Args>(args)...} {
+        requires (sizeof...(Args) <= N && (std::convertible_to<Args, T> && ...))
+    constexpr poly(Args&&... args) : coefs{std::forward<Args>(args)...} {
         DBG_PRINT("variadic constructor called");
     }
 
     template <typename U, std::size_t M>
-    constexpr static auto const_poly(const poly<U,M>& p) {
+    constexpr static auto const_poly(const poly<U, M>& p) {
         DBG_PRINT("const_poly constructor called");
-        return poly<poly<U,M>, 1>{p};
-    }
-    
-    constexpr std::size_t size() const {
-        return N;
-    }
-    
-    constexpr T& operator[](std::size_t i) {
-        return coefs[i];
+        return poly<poly<U, M>, 1>{p};
     }
 
-    constexpr const T& operator[](std::size_t i) const {
-        return coefs[i];
-    }
+    constexpr std::size_t size() const { return N; }
+
+    constexpr T& operator[](std::size_t i) { return coefs[i]; }
+
+    constexpr const T& operator[](std::size_t i) const { return coefs[i]; }
 
     // Copy assignment operator
     template <typename U, std::size_t M>
-    constexpr poly& operator=(const poly<U, M>& other)
-    requires PolyConvertible<U, T, M, N> {
+        requires PolyConvertible<U, T, M, N>
+    constexpr poly& operator=(const poly<U, M>& other) {
+        DBG_PRINT("copy assignment operator called");
         for (std::size_t i = 0; i < M; i++) {
-            coefs[i] = static_cast<T>(other.coefs[i]);
+            coefs[i] = static_cast<T>(other[i]);
         }
         return *this;
     }
 
     // Move assignment operator
     template <typename U, std::size_t M>
-    constexpr poly& operator=(poly<U, M>&& other)
-    requires PolyConvertible<U, T, M, N> {
+        requires PolyConvertible<U, T, M, N>
+    constexpr poly& operator=(poly<U, M>&& other) {
+        DBG_PRINT("move assignment operator called");
         for (std::size_t i = 0; i < M; i++) {
-            coefs[i] = static_cast<T>(std::move(other.coefs[i]));
+            coefs[i] = static_cast<T>(std::move(other[i]));
         }
         return *this;
     }
 
-    // operator+= and operator+ in progress
+    template <typename U, std::size_t M>
+        requires PolyConvertible<U, T, M, N>
+    constexpr poly& operator+=(const poly<U, M>& other) {
+        DBG_PRINT("operator+= for poly called");
+        for (std::size_t i = 0; i < M; i++) {
+            coefs[i] += static_cast<T>(other[i]);
+        }
+        return *this;
+    }
+
+    template <typename U>
+        requires std::convertible_to<U, T>
+    constexpr poly& operator+=(const U& other) {
+        DBG_PRINT("operator+= for U called");
+        coefs[0] += static_cast<T>(other);
+        return *this;
+    }
+    
+    template <typename U, std::size_t M>
+        requires std::is_convertible_v<T, U> || std::is_convertible_v<U, T>
+    constexpr auto operator+(const poly<U, M>& other) const {
+        constexpr std::size_t max_size = std::max(N, M);
+        poly<std::common_type_t<T, U>, max_size> result{};
+        result += *this;
+        result += other;
+        return result;
+    }
 };
 
 // Deduction guide for deducing T and N
--- poly_example.cpp
+++ poly_example.cpp
@@ -4,14 +4,18 @@
 using namespace std;
 
 template <typename T, std::size_t N>
-  constexpr bool operator==(const poly<T, N>& a, const poly<T, N>& b)
-  {
+constexpr bool operator==(const poly<T, N>& a, const poly<T, N>& b) {
     for (std::size_t i = 0; i < N; ++i)
-      if (!(a[i] == b[i]))
-        return false;
+        if (!(a[i] == b[i]))
+            return false;
     return true;
-  }
+}
 
+constexpr poly<int, 2> compile_time_assignment() {
+    poly r(1, 2);
+    r = poly(3, 4); // Assignment at compile-time
+    return r;
+}
 
 int main() {
     // poly<int, 2> p(2137, 6969);
@@ -29,7 +33,7 @@ int main() {
     // poly<int, 2> q(3, 4);
     // poly<int, 2> r = p + q;
     // return 0;
-
+    // ----------------------------------------------
 
     constexpr auto p = poly(2, 1);
     static_assert(p.size() == 2 && p[0] == 2 && p[1] == 1);
@@ -44,11 +48,34 @@ int main() {
     static_assert(q[2][0] == 4.0);
     static_assert(q[2][1] == 0.0);
 
+    static_assert(p + q == poly(poly(3.0, 2.0), 4.0, 4.0));
+
+    static_assert(q + p == poly(poly(3.0, 2.0), 4.0, 4.0));
+
     constexpr auto pp(p);
     static_assert(p == pp);
     constexpr auto qq(q);
     static_assert(q == qq);
 
+    constexpr auto result = compile_time_assignment();
+    static_assert(result[0] == 3 && result[1] == 4);
+
+    poly r(1, 2);
+    poly rr(1, 2, 3, 4);
+    r = poly(3);
+    r = poly(1, 2);
+
+    rr += r;
+    assert(rr[0] == 2 && rr[1] == 4 && rr[2] == 3 && rr[3] == 4);
+
+    constexpr auto x = poly(3, poly(1, 2), 4);
+    static_assert(std::is_same_v<decltype(q), const poly<poly<double, 2>, 3>>);
+    static_assert(x[0][0] == 3.0);
+    static_assert(x[0][1] == 0.0);
+    static_assert(x[1][0] == 1.0);
+    static_assert(x[1][1] == 2.0);
+    static_assert(x[2][0] == 4.0);
+    static_assert(x[2][1] == 0.0);
     
 }
 
